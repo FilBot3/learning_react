a0574ea1708289a13bb82bf1c06ee82ba7252040
Phillp Dudley <Predatorian3@gmail.com>
2022-07-05 12:54:24 -0500

    updated container_entrypoint

--- containers/container_entrypoint.sh
+++ containers/container_entrypoint.sh
@@ -1,42 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ "${CONTAINER_DEBUG}" == "true" ]
-then
-  set -x
-fi
-
-#find /usr/share/nginx/html -type f -name "50*.html" -exec rm -f {} \;
+#set -x
+set -e
 
 cp --recursive /app/* /usr/share/nginx/html
 
-# Check to see that there is a main.js file
-if [ `find /usr/share/nginx/html -type f -name "*.js" -exec ls {} \; | wc -l` -gt 0 ]; then
-  for js_file in `find /usr/share/nginx/html/ -type f -name "*.js"`
+# Get an array/list of the inject_arg_ variables found.
+vars=( $(grep -h -o "inject_arg_[a-zA-Z_0-9-]\+" $(find /usr/share/nginx/html -type f -name "*.js") | sort -u ) )
+for var in "${vars[@]}"
+do
+  echo "Found: ${var}"
+done
+
+for js_file in $(find /usr/share/nginx/html -type f -name "*.js")
+do
+  echo "Replacing ${var} in: ${js_file}"
+
+  for var in "${vars[@]}"
   do
-    var=`sed -e '/.*\"\(inject_arg_[a-zA-Z_09\-]*\)\".*/!d;s//\1/' ${js_file}`
-    # Check to see if var is non-zero length
-    while [[ -n ${var} ]]
-    do
-      #Echo of variable...
-      echo ${var}
-      # Pulling from environment injected into container the value of variable to replace value with in main.js
-      # It's kind of magic, but basically, it's looking to find a variable named whatever the value of var is and
-      # assigning that value to replace.
-      replace=${!var}
-
-      if [[ -z $replace ]]
-      then
-          echo ${var} " will be replaced with an empty string"
-      fi
-
-      #Actual replacement with sed
-      sed -i "s#\"${var}\"#\"${replace}\"#g" ${js_file}
-
-      #checking for next variable to replace
-      var=`sed -e '/.*\"\(inject_arg_[a-zA-Z_09\-]*\)\".*/!d;s//\1/' ${js_file}`
-    done
+    sed --in-place --expression="s|${var}|$(printenv ${var})|g" ${js_file}
   done
-fi
+done
 
 #starting nginx
 nginx -g 'daemon off;'
